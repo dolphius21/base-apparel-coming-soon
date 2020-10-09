605987899b143d8d818e2484cce0091bf2b20be8
dolphius21 <johnrudolphlee@gmail.com>
2020-10-09 21:42:56 +0800

    added email validation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
 const emailInput = document.querySelector("#email");
+const errorIcon = document.querySelector(".error-icon");
+const errorMessage = document.querySelector(".error-message");
+const emailRegex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
+
+function validate(email, regex) {
+  if (regex.test(email.value)) {
+    email.classList.add('valid');
+    email.classList.remove('invalid');
+    errorIcon.style.display = "none";
+    errorMessage.style.display = "none";
+  } else {
+    email.classList.add('invalid')
+    email.classList.remove('valid');
+    errorIcon.style.display = "block";
+    errorMessage.style.display = "block";
+  }
+}
+
+
+emailInput.addEventListener('keyup', (e) => {
+    validate(e.target, emailRegex);
+});
